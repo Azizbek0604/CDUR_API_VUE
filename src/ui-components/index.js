c53a5b96ef134e2cddc9521dbe5648ec81d5a4db
Azizbek0406 <azizbekcoder04@gmail.com>
2024-08-06 18:17:02 +0500

    first commit

---
+++ src/ui-components/index.js
@@ -0,0 +1,6 @@
+import PrimarButton from "@/ui-components/PrimaryButton.vue"
+import Box from "@/ui-components/Box.vue"
+import Input from "@/ui-components/Input.vue"
+import Loader from "@/ui-components/Loader.vue"
+
+export default [PrimarButton, Box, Input, Loader]
